4c092e27dd93b1268a328f6728dafe5760323574
Алексей Манаев <manaev_001@mail.ru>
2022-01-22 19:00:53 +0300

    Реализован контроллер пользователя

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Fitness.BL.Model;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Fitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public User User { get; }
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user">Пользователь.</param>
+        public UserController(User user)
+        {
+            User = user ?? throw new ArgumentNullException("Пользователь не может быть равен null.", nameof(user));
+        }
+        /// <summary>
+        /// Сохранение данных пользователя.
+        /// </summary>
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+        /// <summary>
+        /// Получение данных пользователя.
+        /// </summary>
+        /// <returns>Данные пользователя.</returns>
+        public UserController()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                // TODO: Нужно реализовать чтение пользователя, а так же случай, если его не прочитали
+            }
+        }
+    }
+}
